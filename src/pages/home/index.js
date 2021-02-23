1a1d248b77091c28cc1465b0577cfcaf9f12411f
pzcat <nekobowling@gmail.com>
2021-02-23 21:45:34 +0900

    add home page components(structure only)

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react';
+import Topic from './components/Topic';
+import List from './components/List';
+import Writer from './components/Writer';
+import Recommend from './components/Recommend';
+import {
+  HomeWrapper,
+  HomeMiddle,
+  HomeRight,
+} from './style';
+import washfoot from '../../statics/washfoot.jpg';
 
 class Home extends Component {
   render() {
     return (
-      <div>Home</div>
+      <HomeWrapper>
+        <HomeMiddle>
+          <img className='banner-img' src={washfoot} />
+          <Topic />
+          <List />
+        </HomeMiddle>
+        <HomeRight>
+          <Recommend />
+          <Writer />
+        </HomeRight>
+      </HomeWrapper>
     )
   }
 }
